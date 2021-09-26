994ac1fd7f9c0d4f0004c327cc8423ac2e9280aa
kunal <kunal24bonage@gmail.com>
2021-09-26 16:23:36 +0530

    c1.1 commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Branch cherrypick 02
+         Branch c1.1 commit
         </p>
         <a
           className="App-link"
