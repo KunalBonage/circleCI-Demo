2be866b719214afcab2ec236993149f2a4afbfb3
kunal <kunal24bonage@gmail.com>
2021-09-26 16:14:07 +0530

    c1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {`Learn 01 ${process.env.REACT_APP_ENV}`}
+          {`Learn C1 ${process.env.REACT_APP_ENV}`}
         </a>
       </header>
     </div>
